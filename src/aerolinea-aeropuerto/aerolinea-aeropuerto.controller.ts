30e8c5dc0a7d2f08847917331bf37d8cc092d19c
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2025-05-16 09:31:59 -0500

    feat(aerolinea-aeropuerto): implement controller for managing airport-airline relationships

--- src/aerolinea-aeropuerto/aerolinea-aeropuerto.controller.ts
+++ src/aerolinea-aeropuerto/aerolinea-aeropuerto.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
+import { AerolineaAeropuertoService } from './aerolinea-aeropuerto.service';
+import { AeropuertoDto } from 'src/aeropuerto/aeropuerto.dto';
+import { plainToInstance } from 'class-transformer';
+import { AeropuertoEntity } from '../aeropuerto/aeropuerto.entity';
 
-@Controller('aerolinea-aeropuerto')
-export class AerolineaAeropuertoController {}
+@Controller('airlines')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class AerolineaAeropuertoController {
+  constructor(private readonly aerolineaAeropuertoService: AerolineaAeropuertoService) {}
+
+  @Post(':airlineId/airports/:airportId')
+  async addAirportToAirline(
+    @Param('airlineId') airlineId: string,
+    @Param('airportId') airportId: string,
+  ) {
+    return await this.aerolineaAeropuertoService.addAirportToAirline(airportId, airlineId);
+  }
+
+  @Get(':airlineId/airports')
+  async findAirportsFromAirline(@Param('airlineId') airlineId: string) {
+    return await this.aerolineaAeropuertoService.findAirportsFromAirline(airlineId);
+  }
+
+  @Get(':airlineId/airports/:airportId')
+  async findAirportFromAirline(
+    @Param('airlineId') airlineId: string,
+    @Param('airportId') airportId: string,
+  ) {
+    return await this.aerolineaAeropuertoService.findAirportFromAirline(airportId, airlineId);
+  }
+
+  @Put(':airlineId/airports')
+  async updateAirportsFromAirline(
+    @Param('airlineId') airlineId: string,
+    @Body() aeropuertoDto: AeropuertoDto[],
+  ) {
+    const aeropuertos = plainToInstance(AeropuertoEntity, aeropuertoDto);
+    return await this.aerolineaAeropuertoService.updateAirportsFromAirline(airlineId, aeropuertos);
+  }
+
+  @Delete(':airlineId/airports/:airportId')
+  async deleteAirportFromAirline(
+    @Param('airlineId') airlineId: string,
+    @Param('airportId') airportId: string,
+  ) {
+    return await this.aerolineaAeropuertoService.deleteAirportFromAirline(airportId, airlineId);
+  }
+}
